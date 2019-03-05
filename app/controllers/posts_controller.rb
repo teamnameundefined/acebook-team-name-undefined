3bc10bc3718c9288e4ee1b69a85c6a7188d1d0de
Nikhil Vijayan <nkhilv@gmail.com>
2019-03-05 14:59:28 +0000

    Update/delete features (#17)
    * :construction:Add post update feature
    
    * :construction:Add controller unit tests
    
    * :construction:update controller unit tests
    
    * :construction: controller tests pending
    
    * :construction:wip: update unit tests
    
    * :white_check_mark: :sparkles: add delete feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,59 @@
 class PostsController < ApplicationController
-    def new
-        @post = Post.new
+  before_action :authenticate_user!
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to posts_path
+      flash[:notice] = "Your post has been created!"
+    elsif @post.errors.any?
+      @post.errors.each do |attribute, message|
+     end
+      
+     flash[:alert] = "Your new post couldn't be created! Please check the form."
+     render :new
     end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
     
-    def create
-        @post = Post.create(post_params)
-        redirect_to posts_url
+    if @post.update(post_params)
+      flash[:notice] = "Post was updated"
+      redirect_to posts_path
+    else
+      flash[:notice] = "There was an error saving your post. Please try again."
+      render :edit
     end
-    
-    def index
-        @posts = Post.all.order('created_at DESC')
-    end
-    
-    private
-    
-    def post_params
-        params.require(:post).permit(:message)
-    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to(posts_path)
+  end
+
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:message)
+  end
 end
