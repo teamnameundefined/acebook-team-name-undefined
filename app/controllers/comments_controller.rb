9c251c8b72dd0495f1fffe6931a06861b329e680
Nikhil Vijayan <nkhilv@gmail.com>
2019-03-07 13:43:16 +0000

    :sparkles: add commenting basic feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
-  #sherif
+  
+  # before_action :set_post
+
   def new
     # @post = Post.find(params[:id])
     @comment = Comment.new
@@ -8,27 +10,40 @@ class CommentsController < ApplicationController
   end
 
   def create
+    # @comment = @post.comments.new(params[:comment].permit(:body))
+    p params
+    p 'POOOOOSSTTTT IDDDDDDD'
+    p params[:post_id]
+    p 'JUSSTTTT IDDDDD'
+    p params[:id]
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(params[:comment].permit(:body))
+    @comment = @post.comments.create(comment_params)
+    @comment.user_id = current_user.id
     p 'test'
     p @comment
-    p post_params
-    @comment.user_id = current_user.id
     p 'test2'
     p @comment
     if @comment.save
-      redirect_to post_path(@post)
+      p 'SAVEEEEEDDDD'
+      flash[:success] = "You commented the hell out of that post!"
+      redirect_to posts_path
     else
-      @comment.errors.each do |attribute, message|
-      end
-            
-      flash[:alert] = "Your comment couldn't be created! Please check the form."
-      redirect_to post_path(@post)
+      p 'NOT SAVEEEEDDD'
+      flash[:alert] = "Check the comment form, something went horribly wrong."
+      render root_path
     end
     
   end
-  # Sherif
 
+  def destroy
+    @comment = @post.comments.find(params[:id])
+  
+    @comment.destroy
+    flash[:success] = "Comment deleted :("
+    redirect_to root_path
+  end
+  # Sherif
+  
   # def create
   #   @post = Post.find(params[:post_id])
   #   @comment = @post.comments.new(params[:comment].permit(:body))
@@ -38,13 +53,23 @@ class CommentsController < ApplicationController
   #   elsif @comment.errors.any?
   #     @comment.errors.each do |attribute, message|
   #       end
-      
+  
   #     flash[:alert] = "Your comment couldn't be created! Please check the form."
   #     redirect_to post_path(@post)
   #   end
   # end
-
+  
   private
+  
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def set_post
+    p 'params stufffffffffffff>>>>>>>'
+    p params
+    @post = Post.find(params[:id])
+  end
   
   def post_params
     params.require(:comment).permit(:body)
