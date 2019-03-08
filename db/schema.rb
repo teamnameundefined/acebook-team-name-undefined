f396dc6dd2511850e272c4085803f43b6b37a83d
Ibrahim Butt <ibrahimbutt@me.com>
2019-03-08 13:20:03 +0000

    :lipstick: style new post form

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 20190306140517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "body"
