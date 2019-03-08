90e60e2dd9944f5a7df9b904ebcb0f3d3d4bb4b9
Ibrahim Butt <ibrahimbutt@me.com>
2019-03-08 12:12:34 +0000

    :lipstick: style navigation and alerts

--- spec/features/user_can_log_in_log_out_spec.rb
+++ spec/features/user_can_log_in_log_out_spec.rb
@@ -5,20 +5,20 @@ RSpec.feature "Log in/Log out", type: :feature do
 
   scenario "Can sign up via navigation bar" do
     visit "/"
-    page.find('#sign_up_button').click
-    click_link("Sign up", match: :first)
+    # page.find('#sign_up_button').click
+    click_link("Sign Up", match: :first)
     fill_in "user_email", with: "james@gmail.com"
     fill_in "user_password", with: "password123"
     fill_in "user_password_confirmation", with: "password123"
-    click_button "Sign up"
+    click_button "Sign Up"
     expect(current_path).to eq("/")
-    click_link("Log out")
+    click_link("Log Out")
     expect(current_path).to eq("/users/sign_in")
     fill_in "user_email", with: "james@gmail.com"
     fill_in "user_password", with: "password123"
     check 'user_remember_me'
-    click_button "Log in"
-    expect(page).to have_content("Logged in as james@gmail.com")
+    click_button "Log In"
+    expect(page).to have_content("james@gmail.com")
     expect(current_path).to eq("/")
   end
 end
--- spec/features/user_gets_thrown_error_when_sign_up_wrong_spec.rb
+++ spec/features/user_gets_thrown_error_when_sign_up_wrong_spec.rb
@@ -3,48 +3,48 @@ require 'rails_helper'
 RSpec.feature "Sign Up Errors", type: :feature do
   scenario "Throw error if password too short" do
     visit "/"
-    click_link("Sign up", match: :first)
+    click_link("Sign Up", match: :first)
     fill_in "user_email", with: "james@gmail.com"
     fill_in "user_password", with: "pass"
     fill_in "user_password_confirmation", with: "pass"
-    click_button "Sign up"
+    click_button "Sign Up"
     expect(page).to have_content("Password is too short (minimum is 6 characters)")
   end
 
   scenario "Throw error if email is blank" do
     visit "/"
-    click_link("Sign up", match: :first)
+    click_link("Sign Up", match: :first)
     fill_in "user_password", with: "pass"
     fill_in "user_password_confirmation", with: "pass"
-    click_button "Sign up"
+    click_button "Sign Up"
     expect(page).to have_content("Email can't be blank")
   end
 
   scenario "Throw error if both password fields left blank" do
     visit "/"
-    click_link("Sign up", match: :first)
+    click_link("Sign Up", match: :first)
     fill_in "user_email", with: "james@gmail.com"
-    click_button "Sign up"
+    click_button "Sign Up"
     expect(page).to have_content("Password can't be blank")
   end
 
   scenario "Throw error if passwords don't match" do
     visit "/"
-    click_link("Sign up", match: :first)
+    click_link("Sign Up", match: :first)
     fill_in "user_email", with: "james@gmail.com"
     fill_in "user_password", with: "password1234"
     fill_in "user_password_confirmation", with: "thomas1234"
-    click_button "Sign up"
+    click_button "Sign Up"
     expect(page).to have_content("Password confirmation doesn't match Password")
   end
 
   scenario "Throws error and stays on same route if email is not in vaild format" do
     visit "/"
-    click_link("Sign up", match: :first)
+    click_link("Sign Up", match: :first)
     fill_in "user_email", with: "james.gmail.com"
     fill_in "user_password", with: "password1234"
     fill_in "user_password_confirmation", with: "thomas1234"
-    click_button "Sign up"
+    click_button "Sign Up"
     expect(current_path).to eq("/users")
   end
 
